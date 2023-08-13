7ca554f1f61e9019557e5db6539970ec004f4547
meruem243 <mughees.ahmed243@gmail.com>
2023-08-13 12:03:31 +0500

    adding class for story and StoryBrain class to store story and options

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,76 @@ class Destini extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Destini'),
+          titleTextStyle: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 30.0,
+          ),
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.teal,
+        body: const DestiniBody(),
+      ),
+    );
+  }
+}
+
+class DestiniBody extends StatefulWidget {
+  const DestiniBody({super.key});
+
+  @override
+  State<DestiniBody> createState() => _DestiniBodyState();
+}
+
+class _DestiniBodyState extends State<DestiniBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+      constraints: const BoxConstraints.expand(),
+      child: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Expanded(
+              flex: 6,
+              child: Center(
+                child: Text('Story will show here'),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: MaterialButton(
+                onPressed: () {},
+                color: Colors.blue,
+                child: const Text(
+                  'option 1',
+                  style: TextStyle(fontSize: 25.0, color: Colors.red),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              flex: 1,
+              child: MaterialButton(
+                onPressed: () {},
+                color: Colors.green,
+                child: const Text(
+                  'option 2',
+                  style: TextStyle(fontSize: 25.0, color: Colors.yellow),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/story.dart
@@ -0,0 +1,7 @@
+class Story {
+  String storyLine = 'NULL';
+  String opt1 = 'NULL';
+  String opt2 = 'NULL';
+
+  Story({required this.storyLine, required this.opt1, required this.opt2});
+}
